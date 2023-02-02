390e7a2a382b5dee40fe459946f82cd4fdedf7fd
BLepique <bennetlepique@gmail.com>
2023-02-02 17:20:22 +0100

    Lint

--- webshop/shop/models.py
+++ webshop/shop/models.py
@@ -2,15 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 
+
 class Category(models.Model):
     Category_ID = models.BigAutoField(primary_key=True)
     category_name = models.CharField(max_length=100)
     category_description = models.CharField(max_length=800)
 
+
 class Payment_Method(models.Model):
     Payment_Method_ID = models.BigAutoField(primary_key=True)
     method_name = models.CharField(max_length=100)
     method_fee = models.FloatField()
+
 
 class Address(models.Model):
     Address_ID = models.BigAutoField(primary_key=True)
@@ -18,6 +21,7 @@ class Address(models.Model):
     city = models.CharField(max_length=200)
     street = models.CharField(max_length=100)
     house_number = models.CharField(max_length=10)
+
 
 class Product(models.Model):
     Product_ID = models.BigAutoField(primary_key=True)
@@ -29,10 +33,12 @@ class Product(models.Model):
     date_last_change = models.DateField(auto_now=True)
     delivery_time = models.PositiveIntegerField(min)
 
+
 class Warehouse(models.Model):
     Warehouse_ID = models.BigAutoField(primary_key=True)
     address_id = models.OneToOneField(Address, on_delete=models.CASCADE)
     product_availability = models.ManyToManyField(Product, through='Product_Availability')
+
 
 class Customer(models.Model):
     Customer_ID = models.BigAutoField(primary_key=True)
@@ -46,10 +52,12 @@ class Customer(models.Model):
     cart = models.ManyToManyField(Product, through='Cart', related_name='cart')
     product_likes = models.ManyToManyField(Product, through='Product_Likes', related_name='likes')
 
+
 class Product_Availability(models.Model):
     Product_ID = models.ForeignKey(Product, on_delete=models.CASCADE)
     Warehouse_ID = models.ForeignKey(Warehouse, on_delete=models.CASCADE, related_name='in_warehouse')
     available_amount = models.PositiveIntegerField()
+
 
 class Order(models.Model):
     Order_ID = models.BigAutoField(primary_key=True)
@@ -61,22 +69,26 @@ class Order(models.Model):
     products_per_order = models.ManyToManyField(Product, through='Products_per_Order')
     delivery = models.ManyToManyField(Address, through='Delivery', related_name='delivery')
 
+
 class Products_per_Order(models.Model):
     Order_ID = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='in_the_order')
     Product_ID = models.ForeignKey(Product, on_delete=models.CASCADE)
     order_amount = models.PositiveIntegerField()
     product_price_at_order_time = models.FloatField()
-    
+
+
 class Delivery(models.Model):
     Order_ID = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='deliverys_order_id')
     Delivery_Address_ID = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='delivery_address')
     warehouse_id = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
     delivery_date = models.DateField()
 
+
 class Cart(models.Model):
     Customer_ID = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer')
     Product_ID = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
     cart_amount = models.PositiveIntegerField()
+
 
 class Product_Likes(models.Model):
     Customer_ID = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='liking_customer')
