1b72ea7be4822ef2e6a2e5c141cf29ee0bc728ea
sxllyb <122729172+sxllyb@users.noreply.github.com>
2023-02-11 20:29:11 +0100

    Forms in HTML angepasst

--- webshop/shop/forms.py
+++ webshop/shop/forms.py
@@ -5,8 +5,8 @@ class LoginForm(forms.Form):
     password = forms.PasswordInput(label="Passwort")
 
 class RegisterForm(forms.Form):
-    vorname = forms.CharField(label="Vorname", max_length=100)
-    nachname = forms.CharField(label="Nachname", max_length=100)
+    first_name = forms.CharField(label="Vorname", max_length=100)
+    last_name = forms.CharField(label="Nachname", max_length=100)
     email = forms.EmailInput(label="Email Addresse")
     password = forms.PasswordInput(label="Passwort")
     repeated_password = forms.PasswordInput(label="Wiederhole das Passwort")
