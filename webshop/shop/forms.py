4830afa606660c602f0ec5c2955e4d7d985557ed
sxllyb <122729172+sxllyb@users.noreply.github.com>
2023-02-11 16:03:26 +0100

    Forms

---
+++ webshop/shop/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    email = forms.EmailInput(label="Email Addresse")
+    password = forms.PasswordInput(label="Passwort")
+
+class RegisterForm(forms.Form):
+    vorname = forms.CharField(label="Vorname", max_length=100)
+    nachname = forms.CharField(label="Nachname", max_length=100)
+    email = forms.EmailInput(label="Email Addresse")
+    password = forms.PasswordInput(label="Passwort")
+    repeated_password = forms.PasswordInput(label="Wiederhole das Passwort")
