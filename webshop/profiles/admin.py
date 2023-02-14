f3cceeb10276d214dbb2abb3a08dd5335232ad76
BLepique <bennetlepique@gmail.com>
2023-02-14 15:23:58 +0100

    remigrated Database

--- webshop/profiles/admin.py
+++ webshop/profiles/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Profile
 
 # Register your models here.
 
-admin.site.register(Profile)
+class ProfileAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'username', 'is_active')
+    list_display_links = ('email', 'first_name', 'last')
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Profile, ProfileAdmin)
--- webshop/profiles/models.py
+++ webshop/profiles/models.py
@@ -22,7 +22,7 @@ class ProfileManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_admin(self, first_name, last_name, email, username, password):
+    def create_superuser(self, first_name, last_name, email, username, password):
         user = self.create_user(
             email = self.normalize_email(email),
             username = username,
