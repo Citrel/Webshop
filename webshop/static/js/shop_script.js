c7ce7d031be5fcddf3acd5c239274b32b365021a
sxllyb <122729172+sxllyb@users.noreply.github.com>
2023-02-14 21:37:40 +0100

    like

--- webshop/static/js/shop_script.js
+++ webshop/static/js/shop_script.js
@@ -7,7 +7,7 @@ function plus()
     document.getElementById('quantity').value = result;
 }
 
-function minus(quantity)
+function minus()
 {
     quantity = document.getElementById('quantity').value;
     quantity = parseFloat(quantity);
@@ -20,5 +20,18 @@ function minus(quantity)
         result = quantity - 1;
         result = result.toString();
         document.getElementById('quantity').value = result;
+    }
+}
+
+function like()
+{
+    heart = document.getElementById('button-like-text').innerHTML;
+
+    if(heart == "&#9825;")
+    {
+        document.getElementById('button-like-text').innerHTML = "&#9829;";
+    } else 
+    {
+        document.getElementById('button-like-text').innerHTML = "&#9825;";
     }
 }
