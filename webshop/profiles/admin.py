cb775df070b999ddb4c9436c85c06e75ff777f7e
BLepique <bennetlepique@gmail.com>
2023-02-15 21:33:26 +0100

    pisse

--- webshop/profiles/admin.py
+++ webshop/profiles/admin.py
@@ -9,8 +9,8 @@ class ProfileAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name',
                     'username', 'last_login', 'is_active')
     list_display_links = ('email', 'first_name', 'last_name')
-    readonly_fields = ('last_login',)
-    ordering = ('last_login', )
+    readonly_fields = ('last_login')
+    ordering = ('last_login')
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
