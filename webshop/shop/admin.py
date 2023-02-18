a9c21df5600263ea18941035085f902ed72510cb
BLepique <bennetlepique@gmail.com>
2023-02-18 23:03:00 +0100

    added adminview for Product and Category

--- webshop/shop/admin.py
+++ webshop/shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Category, Product
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'category_description')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'product_description', 'product_image_path', 'price', 'date_last_change', 'delivery_time'
+                    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
