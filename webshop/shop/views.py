7f4117b7d609807e3936e2ede816ee3b9fb98032
Citrel <dajolamb@gmail.com>
2023-02-18 17:14:47 +0100

    liking now changes the heart

--- webshop/shop/views.py
+++ webshop/shop/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from .models import *
 from .forms import *
 from django.db.models import Q, Sum, Count
-import random
+from django import template
+
 
 
 class Homepage:
@@ -12,7 +13,7 @@ class Homepage:
 
         products = Product.objects.all()
         categories = Category.objects.all()
-        cart_item_count = Cart.objects.filter(Customer_ID=request.user).count()
+        cart_item_count = Cart.objects.filter(Customer_ID=request.user.id).count()
         
         return render (request, 'index.html', {'products' : products, 'categories' : categories, 'cart_item_count' : cart_item_count})
     
@@ -21,45 +22,91 @@ class Article():
     
     def show_information(request, pk):
         
+        cart_item_count = Cart.objects.filter(Customer_ID=request.user.id).count()
+        categories = Category.objects.all()
+        
         product = get_object_or_404(Product, pk=pk)
         likes = Product_Likes.objects.filter(Product_ID = pk).count() 
+        likedProduct = Product.objects.get(Product_ID = pk)
+        is_liked = False
         
+        if request.user.is_authenticated:
+            created = Product_Likes.objects.filter(Customer_ID_id= request.user.id, Product_ID = likedProduct).exists()
+            print(1)
+            if not created:
+                is_liked = False
+            else:
+                is_liked = True
+            
         
-        return render(request, 'product_detail.html',{'product' : product, 'likes' : likes})
+        print(is_liked)
+        return render(request, 'product_detail.html',{'product' : product, 'likes' : likes, 'is_liked' : is_liked , 'cart_item_count' : cart_item_count, 'categories' : categories})
     
     def like_product(request, pk):
-              
-        liked = get_object_or_404(Product_Likes, Customer_ID = request.user.id, Product_ID = pk)
         
-        if request.method == 'GET':
-            
-            if liked == None:
-                
-                liked = Product_Likes(Customer_ID=request.user.id, Product_ID=pk)
+        
+        if request.method == 'POST':
+            likedProduct = Product.objects.get(Product_ID = pk)
+        
+            liked, created = Product_Likes.objects.get_or_create(defaults={'Product_ID': likedProduct}, Customer_ID_id= request.user.id, Product_ID = likedProduct)
+        
+        
+            if not created:
+                liked.delete()
+            else:
                 liked.save()
             
-            return redirect('product_detail', pk=pk)
         
-        return render(request, 'product_detail.html')
-                
+            return redirect('details', pk = pk)
+        return render(request,'product_detail.html')
+    
+    
+    def add_to_cart(request, pk):
+        
+        if request.method == 'POST':
+            amount = int(request.POST.get('change_article_amount'))
+            
+            product_obj = Product.objects.get(Product_ID=pk)
+        
+        if amount > 0:
+            created = Cart.objects.filter(Customer_ID = request.user.id, product_key = product_obj).exists()
+        
+            if not created:
+                new_entry = Cart.objects.create(Customer_ID_id = request.user.id, product_key = product_obj, cart_amount = amount)
+                new_entry.save()
+            else:
+                existing_entry = Cart.objects.get(Customer_ID_id = request.user.id, product_key = product_obj)
+                existing_entry.cart_amount += amount
+                existing_entry.save()
+            
+            return redirect('cart')
+        
+        return render(request, 'cart.html')
+    
+    
+    
 
 class Categories:
 
     def show_categories(request, pk):
         
+        cart_item_count = Cart.objects.filter(Customer_ID=request.user.id).count()
+        categories = Category.objects.all()
+        
         category = get_object_or_404(Category, pk=pk)
         
         product_list = Product.objects.filter(category_id__Category_ID__contains=pk)
         
-        return render(request,'category.html', {'category' : category, 'product_list' : product_list})
+        return render(request,'category.html', {'category' : category, 'product_list' : product_list, 'cart_item_count' : cart_item_count, 'categories' : categories})
     
 
 class Search:
 
         def show_searched_products(request):
             
-            results = []
-            
+            cart_item_count = Cart.objects.filter(Customer_ID=request.user.id).count()
+            categories = Category.objects.all()
+                        
             if request.method == "GET":
                 query = request.GET.get('search')
                 
@@ -68,28 +115,94 @@ class Search:
                     
                 result = Product.objects.filter(Q(product_name__icontains=query) | Q(product_description__icontains=query))
                 
-                return render(request, 'search.html', {'query' : query, 'result' : result})
+                return render(request, 'search.html', {'query' : query, 'result' : result, 'cart_item_count' : cart_item_count, 'categories' : categories})
             
            
 class Cart_View:
 
     def show_cart(request):
+        
+        categories = Category.objects.all()
+        cart_item_count = Cart.objects.filter(Customer_ID=request.user.id).count()
 
-        cart_objects = Cart.objects.filter(Customer_ID = request.user)
-        product = Product.objects.filter(Product_ID=Cart.Product_ID)
+        cart_items = Cart.objects.filter(Customer_ID = request.user.id)
+        
 
-        return render(request, 'cart.html', {'product': product, 'cart_objects': cart_objects})
+        payment_sum = 0
+                
+        product = []
+        
+        for cart_item in cart_items:
+           
+            product.append([cart_item.cart_amount, Product.objects.get(Product_ID__contains = cart_item.product_key.Product_ID), 
+                            Product.objects.get(Product_ID__contains = cart_item.product_key.Product_ID).price * cart_item.cart_amount])
+            payment_sum += Product.objects.get(Product_ID__contains = cart_item.product_key.Product_ID).price * cart_item.cart_amount
+        
+        
+        
+
+        return render(request, 'cart.html', {'product': product, 'cart_item_count' : cart_item_count, 'payment_sum' : payment_sum, 'categories' : categories})
+    
+    def increase_cart_amount(request, pk):
+        
+        changing_item = Cart.objects.get(Customer_ID = request.user.id, product_key = pk)
+        changing_item.cart_amount +=1        
+        changing_item.save()
+        
+        return redirect('cart')
+    
+    
+    def decrease_cart_amount(request, pk):
+        
+        changing_item = Cart.objects.get(Customer_ID = request.user.id, product_key = pk)
+        
+        if changing_item.cart_amount == 1:
+            changing_item.cart_amount = 1
+        else:
+            changing_item.cart_amount -= 1
+        changing_item.save()
+        
+        return redirect('cart')
+    
+    def change_cart_amount(request, pk):
+        
+        if request.method == 'GET':
+            new_amount = request.GET.get('change_amount')
+        
+        changing_item = Cart.objects.get(Customer_ID = request.user.id, product_key = pk)
+        
+        if int(new_amount) > 0:
+            changing_item.cart_amount = int(new_amount)
+        
+        changing_item.save()
+        
+        return redirect('cart')
+    
+    def delete_from_cart(request, pk):
+        
+        delete_obj = Cart.objects.get(Customer_ID=request.user.id, product_key=pk)
+        delete_obj.delete()
+        
+        return redirect('cart')
+    
+    
 
 
 class About_Us:
 
     def show_abouts(request):
+        
+        cart_item_count = Cart.objects.filter(Customer_ID=request.user.id).count()
+        categories = Category.objects.all()
 
-        return render(request, 'about_us.html')
+        return render(request, 'about_us.html', {'cart_item_count' : cart_item_count, 'categories' : categories})
 
 
 class Imprint:
 
     def show_imprint(request):
+        
+        cart_item_count = Cart.objects.filter(Customer_ID=request.user.id).count()
+        categories = Category.objects.all()
 
-        return render(request, 'imprint.html')
+        return render(request, 'imprint.html', {'cart_item_count' : cart_item_count, 'categories' : categories})
