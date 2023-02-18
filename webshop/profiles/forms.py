3e6eff8319de2a11251486bfb8fb7a9d2aad8ed6
BLepique <bennetlepique@gmail.com>
2023-02-18 01:53:08 +0100

    Der kleine Mann in meinem Computer hat mir eine Augenkrankheit gegeben

--- webshop/profiles/forms.py
+++ webshop/profiles/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Profile
+from .models import Profile, User_Delivery_Address, User_Payment_Address, User_Credit_Card, User_PayPal, User_Debit
 
 
 class RegistrationForm(forms.ModelForm):
@@ -30,5 +30,72 @@ class RegistrationForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('first_name', 'last_name', 'phone_number')
+
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+class User_Delivery_AddressForm(forms.ModelForm):
+    class Meta:
+        model = User_Delivery_Address
+        fields = ('delivery_street', 'delivery_house_number', 'delivery_city', 'delivery_plz')
+
+    def __init__(self, *args, **kwargs):
+        super(User_Delivery_AddressForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+class User_Payment_AddressForm(forms.ModelForm):
+    class Meta:
+        model = User_Payment_Address
+        fields = ('payment_street', 'payment_house_number', 'payment_city', 'payment_plz')
+
+    def __init__(self, *args, **kwargs):
+        super(User_Payment_AddressForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+class User_Credit_CardForm(forms.ModelForm):
+    class Meta:
+        model = User_Credit_Card
+        fields = ('owner_first_name', 'owner_last_name', 'card_number',
+                  'expiration_date_month', 'expiration_date_year', 'security_code')
+
+    def __init__(self, *args, **kwargs):
+        super(User_Credit_CardForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+class User_PayPalForm(forms.ModelForm):
+    class Meta:
+        model = User_PayPal
+        fields = ('paypal_mail', 'paypal_password')
+
+    def __init__(self, *args, **kwargs):
+        super(User_PayPalForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+class User_DebitForm(forms.ModelForm):
+    class Meta:
+        model = User_Debit
+        fields = ('debit_first_name', 'debit_last_name', 'iban', 'bic')
+
+    def __init__(self, *args, **kwargs):
+        super(User_DebitForm, self).__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
--- webshop/shop/models.py
+++ webshop/shop/models.py
@@ -1,23 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
-from profiles.models import Profile
-
-
-class Creditcard_Information(models.Model):
-    Creditcard_ID = models.BigAutoField(primary_key=True)
-    credit_card_number = models.CharField(max_length=16)
-    expiration_date = models.DateField()
-
-
-class Paypal_Information(models.Model):
-    Paypal_ID = models.BigAutoField(primary_key=True)
-    email = models.CharField(max_length=100)
-
-
-class Debitcard_Information(models.Model):
-    Debitcard_ID = models.BigAutoField(primary_key=True)
-    debitcard_number = models.CharField(max_length=16)
+from profiles.models import Profile, User_Payment_Address, User_Delivery_Address
 
 
 class Category(models.Model):
@@ -32,14 +16,6 @@ class Payment_Method(models.Model):
     method_fee = models.FloatField()
 
 
-class Address(models.Model):
-    Address_ID = models.BigAutoField(primary_key=True)
-    plz = models.CharField(max_length=10)
-    city = models.CharField(max_length=200)
-    street = models.CharField(max_length=100)
-    house_number = models.CharField(max_length=10)
-
-
 class Product(models.Model):
     Product_ID = models.BigAutoField(primary_key=True)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -51,53 +27,19 @@ class Product(models.Model):
     delivery_time = models.PositiveIntegerField(min)
 
 
-class Warehouse(models.Model):
-    Warehouse_ID = models.BigAutoField(primary_key=True)
-    address_id = models.OneToOneField(Address, on_delete=models.CASCADE)
-    product_availability = models.ManyToManyField(
-        Product, through='Product_Availability')
-
-
-class Customer(models.Model):
-    Customer_ID = models.BigAutoField(primary_key=True)
-    user_id = models.OneToOneField(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    current_delivery_address_id = models.ForeignKey(
-        Address, on_delete=models.CASCADE, related_name='current_delivery_address')
-    current_billing_address_id = models.ForeignKey(
-        Address, on_delete=models.CASCADE, related_name='current_billing_address')
-    preferred_payment_method_id = models.ForeignKey(
-        Payment_Method, on_delete=models.CASCADE)
-    creditcard_id = models.ForeignKey(
-        Creditcard_Information, on_delete=models.CASCADE)
-    paypal_id = models.ForeignKey(Paypal_Information, on_delete=models.CASCADE)
-    debitcard_id = models.ForeignKey(
-        Debitcard_Information, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=50)
-
-
-class Product_Availability(models.Model):
-    Product_ID = models.ForeignKey(Product, on_delete=models.CASCADE)
-    Warehouse_ID = models.ForeignKey(
-        Warehouse, on_delete=models.CASCADE, related_name='in_warehouse')
-    available_amount = models.PositiveIntegerField()
-
-
 class Order(models.Model):
     Order_ID = models.BigAutoField(primary_key=True)
-    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
     payment_method_id = models.ForeignKey(
         Payment_Method, on_delete=models.CASCADE)
     payment_address_id = models.ForeignKey(
-        Address, on_delete=models.CASCADE, related_name='payment_address')
+        User_Payment_Address, on_delete=models.CASCADE, related_name='payment_address')
     payed = models.BooleanField()
     order_date = models.DateField(auto_now_add=True)
     products_per_order = models.ManyToManyField(
         Product, through='Products_per_Order')
-    delivery = models.ManyToManyField(
-        Address, through='Delivery', related_name='delivery')
+    delivery = models.ForeignKey(
+        User_Delivery_Address, on_delete=models.CASCADE, related_name='delivery')
 
 
 class Products_per_Order(models.Model):
@@ -112,8 +54,7 @@ class Delivery(models.Model):
     Order_ID = models.ForeignKey(
         Order, on_delete=models.CASCADE, related_name='deliverys_order_id')
     Delivery_Address_ID = models.ForeignKey(
-        Address, on_delete=models.CASCADE, related_name='delivery_address')
-    warehouse_id = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+        User_Delivery_Address, on_delete=models.CASCADE, related_name='delivery_address')
     delivery_date = models.DateField()
 
 
@@ -121,9 +62,8 @@ class Cart(models.Model):
     Customer_ID = models.ForeignKey(
         Profile, on_delete=models.CASCADE, default=None)
     product_key = models.ForeignKey(
-        Product, on_delete=models.CASCADE, related_name='product', default = None)
+        Product, on_delete=models.CASCADE, related_name='product', default=None)
     cart_amount = models.PositiveIntegerField()
-    
 
 
 class Product_Likes(models.Model):
