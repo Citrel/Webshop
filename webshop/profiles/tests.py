0b7188d6461383c2b81d914b3388d40a76f89907
BLepique <bennetlepique@gmail.com>
2023-02-27 17:52:16 +0100

    added more tests

--- webshop/profiles/tests.py
+++ webshop/profiles/tests.py
@@ -9,7 +9,7 @@ class UserTestCase(TestCase):
                                phone_number="02171353244", is_admin=True,
                                is_active=True, is_staff=True, is_superadmin=True)
 
-# after migration
+# AFTER MIGRATION
 #    def test_user_delivery_address(self):
 #        address = User_Delivery_Address.objects.create(user=Profile.objects.get(username="Tester"),
 #                                                       delivery_street="TestStraße", delivery_house_number="99",
@@ -28,3 +28,14 @@ class UserTestCase(TestCase):
                                                      card_number="647283756482936", expiration_date_month="03",
                                                      expiration_date_year="25", security_code="321")
         self.assertEqual(creditcard.__str__(), "Testname")
+
+    def test_user_paypal(self):
+        paypal = User_PayPal.objects.create(user=Profile.objects.get(username="Tester"),
+                                            paypal_mail="testpal@paypal.com", paypal_password="testpass")
+        self.assertEqual(paypal.__str__(), "Testname")
+
+    def test_user_debit(self):
+        debit = User_Debit.objects.create(user=Profile.objects.get(username="Tester"),
+                                          debit_first_name="Testfname", debit_last_name="Testlname",
+                                          iban="DE4768490010048375453", bic="VR5649604")
+        self.assertEqual(debit.__str__(), "Testname")
